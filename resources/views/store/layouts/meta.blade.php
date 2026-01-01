ea865706739a6aac3716428945a96c0c22424a29
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-01-01 11:11:33 +0200

    icon google

--- resources/views/store/layouts/meta.blade.php
+++ resources/views/store/layouts/meta.blade.php
@@ -46,7 +46,9 @@
 {{-- icons --}}
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('store/images/icons/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('store/images/icons/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('store/images/icons/favicon-48x48.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('store/images/icons/favicon-16x16.png') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('store/images/icons/favicon-48x48.png') }}">
 <link rel="manifest" href="{{ asset('store/images/icons/site.webmanifest') }}">
 
 
