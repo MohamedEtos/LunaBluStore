1c6de8c8af95ab07cab5d07d28b8c083c786a9cd
Mohamed Mahrous <69046042+MohamedEtos@users.noreply.github.com>
2026-01-01 12:50:27 +0200

    Update meta.blade.php

--- resources/views/store/layouts/meta.blade.php
+++ resources/views/store/layouts/meta.blade.php
@@ -44,6 +44,7 @@
     </script>
 
 {{-- icons --}}
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('store/images/icons/favicon.ico') }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('store/images/icons/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('store/images/icons/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('store/images/icons/favicon-48x48.png') }}">
